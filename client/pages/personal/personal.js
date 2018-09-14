1b235ff85b0ba07670f2d57742f8481e0658dd6c
sgrjhhh <825132005@qq.com>
2018-09-14 09:50:09 +0800

    个人主页
    添加个人服务类（优惠券、收藏、地址、客服、关于我们）,增加货物运输气泡

--- client/pages/personal/personal.js
+++ client/pages/personal/personal.js
@@ -9,25 +9,64 @@ Page({
     goodsStatus: [{
       "id": "0",
       "url": "../../images/be_payment.png",
-      "status":"待付款"
+      "status": "待付款",
+      "isHandler": false,
+      "handlerNum": "3"
     }, {
       "id": "1",
-        "url": "../../images/be_delivered.png",
-        "status": "待发货"
+      "url": "../../images/be_delivered.png",
+      "status": "待发货",
+      "isHandler": false,
+      "handlerNum": "4"
+
     }, {
       "id": "2",
-        "url": "../../images/be_receipt.png",
-        "status": "待收货"
+      "url": "../../images/be_receipt.png",
+      "status": "待收货",
+      "isHandler": true,
+      "handlerNum": "0"
     }, {
       "id": "3",
-        "url": "../../images/be_evaluated.png",
-        "status": "待评价"
+      "url": "../../images/be_evaluated.png",
+      "status": "待评价",
+      "isHandler": false,
+      "handlerNum": "3"
     }, {
       "id": "4",
-        "url": "../../images/be_return.png",
-        "status": "退换货"
+      "url": "../../images/be_return.png",
+      "status": "退换货",
+      "isHandler": true,
+      "handlerNum": "0"
     }],
-    grids: [1,2,3,4,5,6,7,8,9]
+    personalService: [{
+        "id": "0",
+        "url": "../index/index",
+        "serviceImage": "../../images/coupon.png",
+        "name": "优惠券"
+      }, {
+        "id": "1",
+        "url": "../index/index",
+        "serviceImage": "../../images/favor.png",
+        "name": "收藏"
+      }, {
+        "id": "2",
+        "url": "../index/index",
+        "serviceImage": "../../images/address.png",
+        "name": "地址"
+      },
+      {
+        "id": "3",
+        "url": "../index/index",
+        "serviceImage": "../../images/customer.png",
+        "name": "客服"
+      },
+      {
+        "id": "4",
+        "url": "../index/index",
+        "serviceImage": "../../images/aboutAs.png",
+        "name": "关于我们"
+      },
+    ]
   },
 
   /**
